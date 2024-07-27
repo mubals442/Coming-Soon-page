507735bf6086fdc35e1daf0059bb150a17cd2b98
Musab Alsayed <musab20151@gmail.com>
2024-07-27 20:01:41 +0300

    fix background image

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import NavBar from "./Components/NavBar";
 import SoonMsg from "./Components/SoonMsg";
 import Media from "./Components/Media";
 import back from "../src/assets/backgrounds.jpg";
+import aboveBack from "../src/assets/backing.png";
 
 function App() {
-
   return (
     <div
       dir="ltl"
       className="bg-cover w-full h-full bg-no-repeat bg-opacity-75 bg-pantone"
       style={{ backgroundImage: `url(${back})` }}
     >
-      <div className="bg-cover w-full h-full bg-no-repeat bg-[url('./assets/backing.png')] bg-opacity-50">
+      <div
+        className={`bg-cover w-full h-full bg-no-repeat`}
+        style={{ backgroundImage: `url(${aboveBack})` }}
+      >
         <NavBar />
         <SoonMsg />
         <Media />
